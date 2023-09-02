9684d8d397161525b34506d4bac84e28b0bf6897
andiy <129535413+andriyshykoryak@users.noreply.github.com>
2023-09-02 15:35:27 +0300

    Add files via upload

---
+++ LearningTheC#/calculator/Program.cs
@@ -0,0 +1,27 @@
+namespace calculator
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {   
+            int n1 = int.Parse(Console.ReadLine());
+
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the action");
+            string action = Console.ReadLine();
+
+
+            switch (n1,n2) {
+                case "+":
+
+            
+            }
+
+
+        }
+    }
+}
